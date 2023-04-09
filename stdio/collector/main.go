92a0a7ae6ec756514108f5fcb8b6f524120eb20a
boichique <iuvarov97@gmail.com>
2023-04-09 15:09:51 +0300

    Initial commit

--- stdio/collector/main.go
+++ stdio/collector/main.go
@@ -1,62 +1,13 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"math"
-	"os"
-)
-
-const (
-	noMin = math.MaxInt
-	noMax = math.MinInt
-)
-
 func main() {
 	// create scanner on top of stdin
-	scanner := bufio.NewScanner(os.Stdin)
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
 	// read lines in a loop
-	min, max := noMin, noMax
-	sum := 0
-	counter := 0
-	rangeChanged := false
+	//   parse line
+	//   update stats
+	//   update range and print if changed
 
 	// handler scanner error
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
-
-	for scanner.Scan() {
-		var metric int //   parse line
-		line := scanner.Text()
-		_, err := fmt.Sscanf(line, "metric: %d", &metric)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if metric < min { //   update stats
-			min = metric
-			rangeChanged = true
-		}
-		if metric > max {
-			max = metric
-			rangeChanged = true
-		}
-		counter++
-		sum += metric
-
-		if rangeChanged { //   update range and print if changed
-			fmt.Fprintf(os.Stdout, "New range: [%d:%d]\n", min, max)
-			rangeChanged = false
-		}
-	}
 
 	// print final stats
-	fmt.Fprintf(os.Stdout, "Stats: metrics collected: %d in range [%d:%d] with avg: %s\n", counter, min, max, fmt.Sprintf("%.2f", float64(sum)/float64(counter)))
-
 }
--- stdio/source/main.go
+++ stdio/source/main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
 	"math/rand"
-	"os"
-	"strconv"
-	"time"
 )
 
 const (
@@ -16,47 +10,18 @@ const (
 
 func main() {
 	// flag vars
-	var (
-		interval time.Duration
-		duration time.Duration
-		from     int
-		to       int
-	)
+	var ()
 
 	// define and parse flags
-	flag.DurationVar(&interval, "i", 100*time.Millisecond, "Interval for generation")
-	flag.DurationVar(&duration, "d", 10*time.Second, "Duration of generation")
-	flag.IntVar(&from, "f", 0, "Min range index")
-	flag.IntVar(&to, "t", 100, "Max range index")
-
-	flag.Parse()
 
 	// generator, err := getRandGenerator()
-	generator, err := getRandGenerator()
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// for ; ; {
 	//   generate metrics
 	// {
-	for t := time.Now(); time.Since(t) < duration; time.Sleep(interval) {
-		fmt.Printf("metric: %d\n", generator.Intn(to-from)+from)
-	}
 }
 
 func getRandGenerator() (*rand.Rand, error) {
-	seed, ok := os.LookupEnv(seedEnvVar)
-	if !ok {
-		seed = strconv.Itoa(int(time.Now().UnixNano()))
-	}
-
-	seedInt, err := strconv.Atoi(seed)
-	if err != nil {
-		return nil, fmt.Errorf("can't convert seed into int: %s", err)
-	}
-
-	generator := rand.New(rand.NewSource(int64(seedInt)))
 	// get seed from env var and create generator
-	return generator, nil
+	return nil, nil
 }
